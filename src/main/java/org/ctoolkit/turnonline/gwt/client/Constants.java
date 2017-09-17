355cfbfc470a08bad275e3f334ea1bcff534e625
Aurel Medvegy <aurel.medvegy@ctoolkit.org>
2017-09-17 11:18:24 +0200

    REST_DATE_FORMAT constant has been introduced, a format for encoding and decoding Dates.

---
+++ src/main/java/org/ctoolkit/turnonline/gwt/client/Constants.java
@@ -0,0 +1,14 @@
+package org.ctoolkit.turnonline.gwt.client;
+
+/**
+ * Constants.
+ *
+ * @author <a href="mailto:aurel.medvegy@ctoolkit.org">Aurel Medvegy</a>
+ */
+public interface Constants
+{
+    /**
+     * The format used in REST calls for encoding and decoding Dates.
+     */
+    String REST_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZ";
+}
