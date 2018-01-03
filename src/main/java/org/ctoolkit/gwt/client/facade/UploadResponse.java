43418bfa17ad2aaa99b101d467586cf129776fc3
Aurel Medvegy <aurel.medvegy@ctoolkit.org>
2018-01-03 11:43:36 +0100

    UploadResponse has been added as JSON response wrapper of the BLOB upload operation.

---
+++ src/main/java/org/ctoolkit/gwt/client/facade/UploadResponse.java
@@ -0,0 +1,44 @@
+package org.ctoolkit.gwt.client.facade;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+/**
+ * The JSON response wrapper of the BLOB (binary large object) upload operation.
+ *
+ * @author <a href="mailto:aurel.medvegy@ctoolkit.org">Aurel Medvegy</a>
+ */
+class UploadResponse
+        extends JavaScriptObject
+{
+    protected UploadResponse()
+    {
+    }
+
+    /**
+     * Returns the storage name as full storage path of the uploaded file.
+     *
+     * @return the storage name
+     */
+    public final native String getStorageName() /*-{
+        return this.storageName;
+    }-*/;
+
+    /**
+     * Returns the CDN image static URL if uploaded BLOB represents an image.
+     * Otherwise returns {@code null}.
+     *
+     * @return the image static URL
+     */
+    public final native String getServingUrl() /*-{
+        return this.servingUrl;
+    }-*/;
+
+    /**
+     * Returns the BLOB key as an identification of the uploaded file.
+     *
+     * @return the BLOB key
+     */
+    public final native String getBlobKey() /*-{
+        return this.blobKey;
+    }-*/;
+}
